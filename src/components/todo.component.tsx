96813da5c0bedb13ade5e241fe872983145f7b94
berheg <ink42310@yahoo.com>
2021-09-20 21:48:15 +0200

    todo component added

--- src/components/todo.component.tsx
+++ src/components/todo.component.tsx
@@ -1,27 +1,34 @@
-import React from 'react';
+import React, { ChangeEvent } from 'react';
 import type { TodoType } from '../types/todo.type';
 interface Props {
   todo: TodoType;
-  index: number;
-  removeTodo(index: number): void;
+  removeTodo(id: number): void;
 }
-const Todo = ({ todo, index, removeTodo }: Props) => {
+const Todo = ({ todo, removeTodo }: Props) => {
   const options: string[] = ["Todo", "Doing", "Done"];
   const avaliableOptions = options.filter(oneOption => oneOption !== todo.status);
+  const [selectedOption, setSelectedOption] = React.useState("");
+  const handleSelectChange = (event: ChangeEvent<HTMLSelectElement>): void => {
+    const value = event.target.value;
+    setSelectedOption(value);
+    todo.status = selectedOption;
+    console.log(todo.status);
+
+  };
 
   return (
     <div className="todo">
       {todo.text}
       <div>
-        <select>
-          <option value={todo.status} key={index}>{todo.status}</option>
+        <select value={todo.status} onChange={handleSelectChange}>
+          <option value={todo.status} key={todo.id}>{todo.status}</option>
           {avaliableOptions.map((statusOption, index) => (
             <option value={statusOption} key={index}>{statusOption}</option>
           )
           )}
         </select>
-        <button >{todo.dateString + ", 23:59"}</button>
-        <button onClick={() => removeTodo(index)}>x</button>
+        <span className="deadline">{todo.deadline }</span>
+        <button onClick={() => removeTodo(todo.id)}>x</button>
       </div>
     </div>
   );
