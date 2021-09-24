88d06406c3b75f230d1b19c08e06cdba6826342c
berheg <ink42310@yahoo.com>
2021-09-24 23:51:57 +0200

    todoPage component added

--- src/pages/todoPage.component.tsx
+++ src/pages/todoPage.component.tsx
@@ -7,16 +7,19 @@ import type { TodoType } from '../types/todo.type';
 function TodoPage() {
   const [todos, setTodos] = React.useState<TodoType[]>([
     {
+      id: 1,
       text: "Learn about React",
       status: "Todo",
       deadline: new Date().toDateString()
     },
     {
+      id: 2,
       text: "Meet friend for lunch",
       status: "Todo",
       deadline: new Date().toDateString()
     },
     {
+      id: 3,
       text: "Build really cool todo app",
       status: "Done",
       deadline: new Date().toDateString()
